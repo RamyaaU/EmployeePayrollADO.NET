8e12bacf4d08558b303259a1a9857f47d281e120
Ramya.U <ramyayadav2016@gmail.com>
2020-11-29 13:44:27 +0530

    [Ramya] Add . Added method to insert data into employee payroll

--- EmployeePayrollADO.Net/Program.cs
+++ EmployeePayrollADO.Net/Program.cs
@@ -8,9 +8,47 @@ namespace EmployeePayrollADO.Net
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
-/        static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to EmployeePayroll ADO.Net!");
+            EmployeeRepository repository = new EmployeeRepository();
+            //UC 2
+            repository.GetFullTableDetails();
+
+            //UC 3
+            Console.WriteLine(repository.UpdateSalaryIntoDatabase("Teressa", 30000) ? "Update done successfully " : "Update Failed");
+
+            //UC 5
+            repository.GetEmployeesFromForDateRange("2021 - 02 - 01");
+
+            //UC 6
+            repository.FindGroupedByGenderData();
+
+            //UC 7
+            repository.InsertIntoMultipleTablesWithTransactions();
+        }
+
+        /// <summary>
+        /// Adds the new employee into the database.
+        /// </summary>
+        public static void AddNewEmployee()
+        {
+            EmployeeRepository repository = new EmployeeRepository();
+            EmployeeModel model = new EmployeeModel();
+            model.EmployeeName = "Samantha";
+            model.Address = "chennai";
+            model.BasicPay = 30000;
+            model.Deductions = 4000;
+            model.Department = "Sales";
+            model.Gender = "F";
+            model.PhoneNumber = 983798;
+            model.NetPay = 833;
+            model.Income_Tax = 32;
+            model.StartDate = DateTime.Now;
+            model.TaxablePay = 2300;
+            Console.WriteLine(repository.AddEmployee(model) ? "Record inserted successfully " : "Failed");
         }
     }
 }
+
+      
