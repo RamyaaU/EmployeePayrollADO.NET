ecaa45f9f723a926e6718b4ef73588c97387e2c6
Ramya.U <ramyayadav2016@gmail.com>
2020-11-29 11:53:47 +0530

    [Ramya] Add . Added method for datsbase connection

---
+++ EmployeePayrollADO.Net/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollADO.Net
+{
+    public class EmployeeRepository
+    {
+        //For sql authentication
+        public static string connectionString = @"Data Source=RAMYA\SQLEXPRESS;Initial Catalog=payroll_services;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
