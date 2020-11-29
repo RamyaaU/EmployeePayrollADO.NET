fcde5748f43a13b56ce5b1412723abdc91bca82c
Ramya.U <ramyayadav2016@gmail.com>
2020-11-29 13:34:15 +0530

    [Ramya] Add . Added method to retrieve employees for given date range

--- EmployeePayrollADO.Net/Program.cs
+++ EmployeePayrollADO.Net/Program.cs
@@ -8,9 +8,14 @@ namespace EmployeePayrollADO.Net
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
-/        static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to EmployeePayroll ADO.Net!");
+            EmployeeRepository repository = new EmployeeRepository();
+
+            //UC 5
+            repository.GetEmployeesFromForDateRange("2021 - 02 - 01");
+
         }
     }
 }
