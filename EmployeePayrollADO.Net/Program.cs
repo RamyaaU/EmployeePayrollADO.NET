2d014d35ff5f2577182c6e69168da74a68338166
Ramya.U <ramyayadav2016@gmail.com>
2020-11-29 13:19:37 +0530

    [Ramya] Add . Added method to update salary

--- EmployeePayrollADO.Net/Program.cs
+++ EmployeePayrollADO.Net/Program.cs
@@ -8,9 +8,15 @@ namespace EmployeePayrollADO.Net
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
-/        static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to EmployeePayroll ADO.Net!");
+            //UC 2
+            EmployeeRepository repository = new EmployeeRepository();
+            repository.GetAllEmployees();
+
+            //UC 3
+            Console.WriteLine(repository.UpdateSalaryIntoDatabase("Teressa", 30000) ? "Update done successfully " : "Update Failed");
         }
     }
 }
