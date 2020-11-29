c51f179567d805679ce344fa76f0398308b63b6e
Ramya.U <ramyayadav2016@gmail.com>
2020-11-29 14:00:06 +0530

    [Ramya] Add . Added method to insert multiple values

--- EmployeePayrollADO.Net/Program.cs
+++ EmployeePayrollADO.Net/Program.cs
@@ -8,9 +8,33 @@ namespace EmployeePayrollADO.Net
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
-/        static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to EmployeePayroll ADO.Net!");
+            EmployeeRepository repository = new EmployeeRepository();
+
+            //UC 8 
+            repository.RetrieveEmployeeDetailsFromMultipleTables();
+        }
+
+        /// Adds the new employee into the database.
+        public static void AddNewEmployee()
+        {
+            EmployeeRepository repository = new EmployeeRepository();
+            EmployeeModel model = new EmployeeModel();
+            model.EmployeeName = "sam";
+            model.Address = "chennai";
+            model.BasicPay = 50000;
+            model.Deductions = 4000;
+            model.Department = "Sales";
+            model.Gender = "M";
+            model.PhoneNumber = 983798;
+            model.NetPay = 66000;
+            model.Income_Tax = 3200;
+            model.StartDate = DateTime.Now;
+            model.TaxablePay = 3200;
+            Console.WriteLine(repository.AddEmployee(model) ? "Record inserted successfully " : "Failed");
         }
     }
 }
+      
